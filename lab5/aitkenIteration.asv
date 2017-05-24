5adf3d22f64e577e40e38a8e579e23e9ffa6a76c
ALISA19 <isabelaconstantin19@gmail.com>
2017-05-24 11:57:23 +0200

    ex1.1

---
+++ lab5/aitkenIteration.asv
@@ -0,0 +1,5 @@
+function [root, flag, convHist_res, convHist_x] = aitkenIteration(f, c, x0, 1, depth-1)
+
+
+end
+
---
+++ lab5/mainEx1.m
@@ -0,0 +1,7 @@
+ f = @(x) log(x*exp(x));
+ c = -3/4;
+ x_0 = 3;
+ tol = 1e-12;
+ maxIt = 25;
+ 
+ [root, flag, convHist_res, convHist_x] = staticIteration_EXAMPLE(f,c, x_0, tol, maxIt);
