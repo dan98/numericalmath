99238c9194a9a831793f326d5e91145a1dcbcc21
ALISA19 <isabelaconstantin19@gmail.com>
2017-06-06 11:30:49 +0200

    message

--- lab6/main1.m
+++ lab6/main1.m
@@ -1,16 +1,24 @@
-l = -100;
+close all
+l = -1;
 hf = @(t, y) (l*(y - sin(t)) + cos(t));
 hdf = @(t, y) (l);
 
 hex = @(t) (exp(l*t) + sin(t));
-
-[rsx, rsu] = odeSolveTheta(hf, [0, 10], 1, hdf, 1/2, 0.001);
+h = zeros(1,7);
+plot(rsx, rsexact);
+hold on;
+col = ['-c', '-r', '-b','-c', '-r', '-b','-g' ];
+for i=1:7
+    %h(i) = 2 * 0.01 + (i-4) * 1e-4;
+    h(i) = 2^(-i);
+    [rsx, rsu] = odeSolveTheta(hf, [0, 10], 1, hdf, 1, h(i));
+    plot(rsx, rsu,col(i) , 'DisplayName', sprintf('%f', h(i)) );
+    
+end
+legend('show');
 
 rsexact = rsu;
 for i=1:length(rsu)
   rsexact(i) = hex(rsx(i));
 end
 
-plot(rsx, rsu);
-hold on;
-plot(rsx, rsexact);
